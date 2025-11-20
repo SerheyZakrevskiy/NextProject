4128763b92447c371d293bc83df0686817b39b40
Sergiliy <serhey781@gmail.com>
2025-11-20 13:43:48 +0200

    created desgn for logo site

--- src/components/UI/header.tsx
+++ src/components/UI/header.tsx
@@ -1,17 +1,19 @@
 "use client"
 
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+import Image from "next/image";
 
-export const AcmeLogo = () => {
+export const Logo = () => {
   return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
+    <Image
+    src="/logo.png"
+    alt="Ukrainian cuisine"
+    width={30}
+    height={30}
+    priority
+    />
+    
+
   );
 };
 
@@ -19,7 +21,7 @@ export default function Header() {
   return (
     <Navbar>
       <NavbarBrand>
-        <AcmeLogo />
+        <Logo />
         <p className="font-bold text-inherit">ACME</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
