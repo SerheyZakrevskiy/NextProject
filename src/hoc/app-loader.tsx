4c1a99e838e97ad8595ac9d22bd79b2d597f595d
Sergiliy <serhey781@gmail.com>
2025-11-29 15:42:01 +0200

    Added the ability to add and delete

--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAuthStore } from "@/store/auth.store";
+import { useIngredientStore } from "@/store/ingredient.store";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
@@ -10,11 +11,20 @@ interface IProps {
 
 const AppLoader = ({ children }: IProps) => {
   const { data: session, status } = useSession();
-  const { setAuthState } = useAuthStore();
+  const { ingredients, loadIngredients } = useIngredientStore();
+  const { isAuth, setAuthState } = useAuthStore();
+
+  console.log("ingredietns", ingredients);
 
   useEffect(() => {
     setAuthState(status, session);
   }, [status, session, setAuthState]);
+
+  useEffect(() => {
+    if (isAuth) {
+      loadIngredients();
+    }
+  });
 
   return <>{children}</>;
 };
--- src/store/ingredient.store.ts
+++ src/store/ingredient.store.ts
@@ -1,3 +1,8 @@
+import {
+  createIngredient,
+  deleteIngredient,
+  getIngredients,
+} from "@/actions/ingredient";
 import { IIngredient } from "@/types/ingredient";
 import { create } from "zustand";
 
@@ -5,9 +10,71 @@ interface IngredientState {
   ingredients: IIngredient[];
   isLoading: boolean;
   error: string | null;
-  LoadIngredients: () => Promise<void>;
+  loadIngredients: () => Promise<void>;
   addIngredient: (FormData: FormData) => Promise<void>;
   removeIngredient: (id: string) => Promise<void>;
 }
 
-export const useIngredientStore = create<IngredientState>;
+export const useIngredientStore = create<IngredientState>((set) => ({
+  ingredients: [],
+  isLoading: false,
+  error: null,
+
+  loadIngredients: async () => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const result = await getIngredients();
+
+      if (result.success) {
+        set({ ingredients: result.ingredients, isLoading: false });
+      } else {
+        set({ error: result.error, isLoading: false });
+      }
+    } catch (error) {
+      console.log("error", error);
+      set({ error: "Error is Loading ingredients", isLoading: false });
+    }
+  },
+
+  addIngredient: async (FormData: FormData) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const result = await createIngredient(FormData);
+
+      if (result.success) {
+        set((state) => ({
+          ingredients: [...state.ingredients, result.ingredient],
+          isLoading: false,
+        }));
+      } else {
+        set({ error: result.error, isLoading: false });
+      }
+    } catch (error) {
+      console.log("error", error);
+      set({ error: "Error is Added ingredients", isLoading: false });
+    }
+  },
+  removeIngredient: async (id: string) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const result = await deleteIngredient(id);
+
+      if (result.success) {
+        set((state) => ({
+          ingredients: state.ingredients.filter(
+            (ingredient) => ingredient.id !== id
+          ),
+          isLoading: false,
+        }));
+      } else {
+        set({ error: result.error, isLoading: false });
+      }
+    } catch (error) {
+      console.log("error", error);
+      set({ error: "Error is Added ingredients", isLoading: false });
+    }
+  },
+}));
